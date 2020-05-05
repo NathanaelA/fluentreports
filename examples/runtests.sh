63fb9daf79db0acba4d9f31a045776d0c9886562
Nathanael Anderson <nathan@master-technology.com>
2020-05-04 22:58:32 -0500

    Another update for testing

--- examples/runtests.sh
+++ examples/runtests.sh
@@ -5,11 +5,13 @@ TESTING=true
 
 pushd examples
 
+echo Starting Rendering Tests > /tmp/results.txt
 # Run all the test
 for f in demo*.js ; do
   node $f
   if [[ ! $? -eq 0 ]]; then
     echo Failed $f
+    echo Failed $f >> /tmp/results.txt
     exit 1
   fi
 done;
