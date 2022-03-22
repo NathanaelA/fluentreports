NathanaelA/fluentreports

commit 8d0e925beea715fcccb8d7b4af8d557ddf9ef78b

Fix font size field wipe (#257)
* Should fix #248 now values cannot be saved as an invalid value.

* Since typeof null is "object" made sure to properly check for if something is null.

generator/fluentReportsGenerator.js
@@ -3695,8 +3695,8 @@ class frElement { // jshint ignore:line
                     continue;
                 }
 
-                // If the item is undefined, then we don't bother saving it.
-                if (typeof this[curProp.field] === 'undefined') {
+                // If the item is undefined null or NaN, then we don't bother saving it.
+                if (typeof this[curProp.field] === 'undefined' || this[curProp.field] === null || isNaN(this[curProp.field])) {
                     continue;
                 }
 

commit 4f41eb2e42b762724acc3269e3af6c6f71294984

Fix issue with Saving caused by #257

generator/fluentReportsGenerator.js
@@ -3696,7 +3696,11 @@ class frElement { // jshint ignore:line
                 }
 
                 // If the item is undefined null or NaN, then we don't bother saving it.
-                if (typeof this[curProp.field] === 'undefined' || this[curProp.field] === null || isNaN(this[curProp.field])) {
+                if (typeof this[curProp.field] === 'undefined' || this[curProp.field] === null) {
+                    continue;
+                }
+
+                if (typeof this[curProp.field] === 'number' && isNaN(this[curProp.field])) {
                     continue;
                 }
 
@@ -4414,20 +4418,20 @@ class frImage extends frTitledElement { // jshint ignore:line
                 display: this._createFitSelect.bind(this),
                 destination: 'settings'
             },
-            {
+           /* {
                 type: 'select',
                 field: "align",
                 default: "left",
                 display: this._createAlignSelect.bind(this),
                 destination: 'settings'
-            },
-            {
+            }, */
+           /* {
                 type: 'select',
                 field: "valign",
                 default: "top",
                 display: this._createVAlignSelect.bind(this),
                 destination: 'settings'
-            },
+            }, */
             {
                 type: 'number',
                 title: 'scale',